cbf0bb91e0cced79f523df5760887056914f742a
MelaineF <melaine.favennec@gmail.com>
2022-09-22 16:33:21 +0200

    mel commit

--- Assets/Display/GridDisplay.cs
+++ Assets/Display/GridDisplay.cs
@@ -52,7 +52,7 @@ public class GridDisplay : MonoBehaviour
 
                 
               for(int i =0; i < width; i++){
-                board[0][i] = (Oblock.getModel1())[i];
+                board[0][i] = (oblock.getModel1())[i];
               }
 
        // }
